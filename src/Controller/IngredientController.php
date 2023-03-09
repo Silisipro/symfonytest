db0a0f843b7b44817a7634273ea9a086fb7d119f
Sylvestre HONFO <sylvestrehonfo4@gmail.com>
2023-03-09 22:05:27 +0100

    ok

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
- {
-    #[Route('/', 'home.index', methods:['GET'])]
+class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'app_ingredient')]
     public function index(): Response
     {
-        return $this->render('pages/home.html.twig');
+        return $this->render('pages/ingredient/index.html.twig', [
+            'controller_name' => 'IngredientController',
+        ]);
     }
 }
-
-
-
-?>
